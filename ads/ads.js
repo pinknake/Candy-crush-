bca72a1c1d020053c0c2675122d68177d2afd6f9
Ankush Saggu <ankush22004@gmail.com>
2026-03-10 11:25:54 +0530

    Create ads.js

---
+++ ads/ads.js
@@ -0,0 +1,14 @@
+document.write(`
+<script src="https://tonicgoverness.com/ef/1a/43/ef1a432319118bcb45467ce0fd7f2ff4.js"><\/script>
+
+<script>
+atOptions={
+'key':'2839a3c1ca8fc9e07589a1b83ea67a15',
+'format':'iframe',
+'height':50,
+'width':320
+}
+<\/script>
+
+<script src="https://tonicgoverness.com/2839a3c1ca8fc9e07589a1b83ea67a15/invoke.js"><\/script>
+`)
